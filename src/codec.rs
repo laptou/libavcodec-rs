f46fccfc449855fe3163a5a88bd1c71e5196a192
Ibiyemi Abiodun <ibiyemi@intulon.com>
2025-03-15 00:58:02 -0400

    new codec + format abstractions

--- src/codec.rs
+++ src/codec.rs
@@ -8,6 +8,7 @@ use crate::packet::Packet;
 use libavcodec_sys as sys;
 use num_traits::FromPrimitive;
 use std::ptr;
+use std::ptr::NonNull;
 
 pub struct Codec {
     inner: *const sys::AVCodec,
@@ -31,69 +32,78 @@ impl Codec {
 }
 
 pub struct CodecContext {
-    inner: *mut sys::AVCodecContext,
+    inner: NonNull<sys::AVCodecContext>,
 }
 
 unsafe impl Send for CodecContext {}
 
+impl AsRef<sys::AVCodecContext> for CodecContext {
+    fn as_ref(&self) -> &sys::AVCodecContext {
+        unsafe { self.inner.as_ref() }
+    }
+}
+
+impl AsMut<sys::AVCodecContext> for CodecContext {
+    fn as_mut(&mut self) -> &mut sys::AVCodecContext {
+        unsafe { self.inner.as_mut() }
+    }
+}
+
 impl CodecContext {
     pub fn new(codec: &Codec) -> Result<Self> {
         let inner = unsafe { sys::avcodec_alloc_context3(codec.as_ptr()) };
-        if inner.is_null() {
-            return Err(FFmpegError::new(-1));
-        }
+        let inner = NonNull::new(inner).ok_or(FFmpegError::new(-1))?;
+
         Ok(CodecContext { inner })
     }
 
     pub fn as_mut_ptr(&mut self) -> *mut sys::AVCodecContext {
-        self.inner
+        self.inner.as_ptr()
     }
 
     pub fn as_ptr(&self) -> *const sys::AVCodecContext {
-        self.inner
+        self.inner.as_ptr()
     }
 
     pub fn width(&self) -> usize {
-        unsafe { (*self.inner).width as usize }
+        self.as_ref().width as usize
     }
 
     pub fn height(&self) -> usize {
-        unsafe { (*self.inner).height as usize }
+        self.as_ref().height as usize
     }
 
     /// The pixel format of the video.  
     pub fn pixel_format(&self) -> AVPixelFormat {
-        AVPixelFormat::from_i32(unsafe { (*self.inner).pix_fmt }).unwrap()
+        AVPixelFormat::from_i32(self.as_ref().pix_fmt).unwrap()
     }
 
     /// The sample format of the audio.
     pub fn sample_format(&self) -> AVSampleFormat {
-        AVSampleFormat::from_i32(unsafe { (*self.inner).sample_fmt }).unwrap()
+        AVSampleFormat::from_i32(self.as_ref().sample_fmt).unwrap()
     }
 
     /// The sample rate of the audio.
     pub fn sample_rate(&self) -> usize {
-        unsafe { (*self.inner).sample_rate as usize }
+        self.as_ref().sample_rate as usize
     }
 
     /// The number of audio channels.
     pub fn channel_count(&self) -> usize {
-        unsafe { (*self.inner).ch_layout.nb_channels as usize }
+        self.as_ref().ch_layout.nb_channels as usize
     }
 
     /// The number of audio samples per channel in an audio frame.
     pub fn frame_size(&self) -> usize {
-        unsafe { (*self.inner).frame_size as usize }
+        self.as_ref().frame_size as usize
     }
 
     pub fn set_skip_frame(&mut self, value: AVDiscard) {
-        unsafe {
-            (*self.inner).skip_frame = value as i32;
-        }
+        self.as_mut().skip_frame = value as i32;
     }
 
     pub fn open(&mut self, codec: &Codec) -> Result<()> {
-        let ret = unsafe { sys::avcodec_open2(self.inner, codec.as_ptr(), ptr::null_mut()) };
+        let ret = unsafe { sys::avcodec_open2(self.as_mut_ptr(), codec.as_ptr(), ptr::null_mut()) };
         if ret < 0 {
             Err(FFmpegError::new(ret))
         } else {
@@ -102,7 +112,7 @@ impl CodecContext {
     }
 
     pub fn send_packet(&mut self, packet: &Packet) -> Result<()> {
-        let ret = unsafe { sys::avcodec_send_packet(self.inner, packet.as_ptr()) };
+        let ret = unsafe { sys::avcodec_send_packet(self.as_mut_ptr(), packet.as_ptr()) };
         if ret < 0 {
             Err(FFmpegError::new(ret))
         } else {
@@ -111,19 +121,23 @@ impl CodecContext {
     }
 
     pub fn receive_frame(&mut self, frame: &mut Frame) -> Result<()> {
-        let ret = unsafe { sys::avcodec_receive_frame(self.inner, frame.as_mut_ptr()) };
+        let ret = unsafe { sys::avcodec_receive_frame(self.as_mut_ptr(), frame.as_mut_ptr()) };
         if ret < 0 {
             Err(FFmpegError::new(ret))
         } else {
             Ok(())
         }
     }
+
+    pub fn time_base(&self) -> sys::AVRational {
+        self.as_ref().time_base
+    }
 }
 
 impl Drop for CodecContext {
     fn drop(&mut self) {
         unsafe {
-            sys::avcodec_free_context(&mut self.inner);
+            sys::avcodec_free_context(&mut self.as_mut_ptr());
         }
     }
 }
--- src/format.rs
+++ src/format.rs
@@ -1,16 +1,28 @@
+use crate::Stream;
 use crate::error::{FFmpegError, Result};
 use crate::packet::Packet;
-use crate::Stream;
 use libavcodec_sys as sys;
 use std::ffi::CString;
 use std::path::Path;
-use std::ptr;
+use std::ptr::{self, NonNull};
 
 pub struct FormatContext {
-    inner: *mut sys::AVFormatContext,
+    inner: NonNull<sys::AVFormatContext>,
 }
 
 unsafe impl Send for FormatContext {}
+
+impl AsRef<sys::AVFormatContext> for FormatContext {
+    fn as_ref(&self) -> &sys::AVFormatContext {
+        unsafe { self.inner.as_ref() }
+    }
+}
+
+impl AsMut<sys::AVFormatContext> for FormatContext {
+    fn as_mut(&mut self) -> &mut sys::AVFormatContext {
+        unsafe { self.inner.as_mut() }
+    }
+}
 
 impl FormatContext {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self> {
@@ -32,31 +44,37 @@ impl FormatContext {
         }
 
         let ret = unsafe { sys::avformat_find_stream_info(inner, ptr::null_mut()) };
+
         if ret < 0 {
             unsafe { sys::avformat_close_input(&mut inner) };
             return Err(FFmpegError::new(ret));
         }
 
+        let inner = NonNull::new(inner).unwrap();
+
         Ok(FormatContext { inner })
     }
 
     pub fn as_mut_ptr(&mut self) -> *mut sys::AVFormatContext {
-        self.inner
+        self.inner.as_ptr()
     }
 
     pub fn as_ptr(&self) -> *const sys::AVFormatContext {
-        self.inner
+        self.inner.as_ptr()
     }
 
     pub fn streams(&self) -> impl Iterator<Item = Stream> {
-        let nb_streams = unsafe { (*self.inner).nb_streams };
+        let nb_streams = self.as_ref().nb_streams;
+
         let streams =
-            unsafe { std::slice::from_raw_parts((*self.inner).streams, nb_streams as usize) };
+            unsafe { std::slice::from_raw_parts(self.as_ref().streams, nb_streams as usize) };
+
         streams.iter().map(|&ptr| Stream { inner: ptr })
     }
 
     pub fn read_packet(&mut self, packet: &mut Packet) -> Result<bool> {
-        let ret = unsafe { sys::av_read_frame(self.inner, packet.as_mut_ptr()) };
+        let ret = unsafe { sys::av_read_frame(self.inner.as_ptr(), packet.as_mut_ptr()) };
+
         if ret < 0 {
             if ret == sys::AVErrorEof {
                 Ok(false)
@@ -67,12 +85,105 @@ impl FormatContext {
             Ok(true)
         }
     }
+
+    pub fn output<P: AsRef<Path>>(path: P, format: Option<&str>) -> Result<Self> {
+        let path_str = path.as_ref().to_str().ok_or(FFmpegError::new(-1))?;
+        let path_cstr = CString::new(path_str).map_err(|_| FFmpegError::new(-1))?;
+        let format_cstr = format.map(|f| CString::new(f).unwrap());
+
+        let mut ctx = std::ptr::null_mut();
+        let ret = unsafe {
+            sys::avformat_alloc_output_context2(
+                &mut ctx,
+                std::ptr::null(),
+                format_cstr
+                    .as_ref()
+                    .map(|s| s.as_ptr())
+                    .unwrap_or(std::ptr::null()),
+                path_cstr.as_ptr(),
+            )
+        };
+
+        if ret < 0 {
+            return Err(FFmpegError::new(ret));
+        }
+
+        let ctx = NonNull::new(ctx).ok_or(FFmpegError::new(-1))?;
+
+        // Open output file if needed
+        if unsafe { (*ctx.as_ptr()).oformat.as_ref() }
+            .map(|f| (f.flags & sys::AVFMT_NOFILE as i32) == 0)
+            .unwrap_or(false)
+        {
+            let ret = unsafe {
+                sys::avio_open(
+                    &mut (*ctx.as_ptr()).pb,
+                    path_cstr.as_ptr(),
+                    sys::AVIO_FLAG_WRITE as i32,
+                )
+            };
+            if ret < 0 {
+                unsafe { sys::avformat_free_context(ctx.as_ptr()) };
+                return Err(FFmpegError::new(ret));
+            }
+        }
+
+        Ok(FormatContext { inner: ctx })
+    }
+
+    pub fn write_header(&mut self) -> Result<()> {
+        let ret = unsafe { sys::avformat_write_header(self.inner.as_ptr(), std::ptr::null_mut()) };
+        if ret < 0 {
+            Err(FFmpegError::new(ret))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn write_frame(&mut self, packet: &mut Packet) -> Result<()> {
+        let ret = unsafe { sys::av_write_frame(self.inner.as_ptr(), packet.as_mut_ptr()) };
+        if ret < 0 {
+            Err(FFmpegError::new(ret))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn write_frame_interleaved(&mut self, packet: &mut Packet) -> Result<()> {
+        let ret =
+            unsafe { sys::av_interleaved_write_frame(self.inner.as_ptr(), packet.as_mut_ptr()) };
+        if ret < 0 {
+            Err(FFmpegError::new(ret))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn write_trailer(&mut self) -> Result<()> {
+        let ret = unsafe { sys::av_write_trailer(self.inner.as_ptr()) };
+        if ret < 0 {
+            Err(FFmpegError::new(ret))
+        } else {
+            Ok(())
+        }
+    }
 }
 
 impl Drop for FormatContext {
     fn drop(&mut self) {
         unsafe {
-            sys::avformat_close_input(&mut self.inner);
+            // sys::avformat_close_input(&mut self.inner);
+
+            if let Some(fmt) = self.as_ref().oformat.as_ref() {
+                if (fmt.flags & sys::AVFMT_NOFILE as i32) == 0 {
+                    let mut pb = self.as_mut().pb;
+                    if !pb.is_null() {
+                        sys::avio_closep(&mut pb);
+                    }
+                }
+            }
+
+            sys::avformat_free_context(self.as_mut_ptr());
         }
     }
 }
